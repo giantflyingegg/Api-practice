aef67781d13c1ac54a4afae8c63fbe68d5053a8e
giantflyingegg <abwaham@gmail.com>
2023-10-23 17:52:19 +0100

    Working postcode checker needs styling

---
+++ js/index.js
@@ -0,0 +1,24 @@
+async function lookupPostcode() {
+    //get postcode from input field
+    const postcode = document.getElementById('postcode').value;
+    try {
+        const response = await fetch(`http://localhost:3000/api/postcode?postcode=${postcode}`);
+        const data = await response.json();
+
+        //extract data from response
+        const region = data.result.region;
+        const pfa = data.result.pfa;
+        const latitude = data.result.latitude;
+        const longitude = data.result.longitude;
+
+        //target output fields
+        document.getElementById('regResult').value = region || '';
+        document.getElementById('pfaResult').value = pfa || '';
+        document.getElementById('latResult').value = latitude || '';
+        document.getElementById('lonResult').value = longitude || '';
+    } 
+        //catch errors
+        catch (error) {
+        console.error('Error fetching postcode data', error);
+        }
+}
---
+++ js/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use(express.static('public')); // To serve static files
+
+app.get('/api/postcode', async (req, res) => {
+  try {
+    const postcode = req.query.postcode;
+    const response = await axios.get(`http://api.postcodes.io/postcodes/${postcode}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching postcode data', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const axios = require('axios');
+// const app = express();
+
+// app.use(express.static('public'));
+
+// app.get('/postcode', async (req, res) => {
+//     try {
+//         const postcode = req.query.postcode;
+//         if (!postcode) {
+//           return res.status(400).send('Postcode is required');
+//     }
+
+//     const response = await axios.get(`http://api.postcodes.io/postcodes/${postcode}`);
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
