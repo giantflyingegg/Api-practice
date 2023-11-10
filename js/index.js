3ccdefa7ba07da1388d6762c7afe91a4c6b249d5
giantflyingegg <abwaham@gmail.com>
2023-11-10 11:10:02 +0000

    redoing server

--- js/index.js
+++ js/index.js
@@ -1,30 +1,37 @@
 async function lookupPostcode() {
-    const postcode = document.getElementById('postcode').value;
-    try {
-      const response = await fetch(`https://api-practice-pixw.onrender.com/api/postcode?postcode=${postcode}`);
-      const data = await response.json();
-      const { region, pfa, latitude, longitude } = data.result;
-      document.getElementById('regResult').textContent = region;
-      document.getElementById('pfaResult').textContent = pfa;
-      document.getElementById('latResult').textContent = latitude;
-      document.getElementById('lonResult').textContent = longitude;
-  
-      // Fetch crime data
-      const date = '2023-01'; // Set this to the date you want to query
-      const crimeResponse = await fetch(`https://api-practice-pixw.onrender.com/api/crime?date=${date}&lat=${latitude}&lng=${longitude}`);
-      const crimeData = await crimeResponse.json();
-      displayCrimes(crimeData);
-    } catch (error) {
-      console.error('Error fetching data', error);
-    }
+  // Target inputs
+  const postcode = document.getElementById('postcode').value;
+  const dateSelection = document.getElementById('dateSelector').value;
+
+  try {
+    const response = await fetch(`https://api-practice-pixw.onrender.com/api/postcode?postcode=${postcode}`);
+    const data = await response.json();
+    const { region, pfa, latitude, longitude } = data.result;
+    document.getElementById('regResult').textContent = region;
+    document.getElementById('pfaResult').textContent = pfa;
+    document.getElementById('latResult').textContent = latitude;
+    document.getElementById('lonResult').textContent = longitude;
+
+    // Fetch crime data
+    const date = dateSelection; 
+    const crimeResponse = await fetch(`https://api-practice-pixw.onrender.com/api/crime?date=${date}&lat=${latitude}&lng=${longitude}`);
+    const crimeData = await crimeResponse.json();
+    displayCrimes(crimeData);
+  } catch (error) {
+    console.error('Error fetching data', error);
   }
-  
-  function displayCrimes(crimes) {
-    const container = document.getElementById('crimeData');
-    container.innerHTML = ''; // Clear previous results
-    crimes.forEach(crime => {
+}
+
+function displayCrimes(crimes) {
+  const container = document.getElementById('crimeData');
+  container.innerHTML = ''; // Clear previous results
+  crimes.forEach(crime => {
+    if (crime.location && crime.location.street && crime.outcome_status) {
       const div = document.createElement('div');
-      div.textContent = `${crime.category}: ${crime.location_type} at ${crime.location.street.name}`;
+      div.textContent = `${crime.month} ${crime.category}: ${crime.location.street.name} resulting in: ${crime.outcome_status.category} on ${crime.outcome_status.date}`;
       container.appendChild(div);
-    });
-  }
+    } else {
+      console.error('Incomplete crime data:', crime);
+    }
+  });
+}
--- js/server.js
+++ js/server.js
@@ -1,36 +1,78 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const app = express();
+// Import necessary modules
+const express = require('express'); // Express framework to handle HTTP requests
+const axios = require('axios'); // Axios for making HTTP requests to external APIs
+const cors = require('cors'); // CORS middleware to enable cross-origin requests
+const app = express(); // Create an instance of an express application
 
+// Middleware to enable CORS for all routes
 app.use(cors());
 
-app.use(express.static('public')); // To serve static files
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
+// Logging middleware to log all incoming requests to the server
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next(); // Call next to continue processing the request
+});
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  console.error(error.stack); // Logs stack trace
+  res.status(500).send('Something broke!');
+});
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] Request from ${req.ip}: ${req.method} ${req.path}`);
+  console.log(`User-Agent: ${req.headers['user-agent']}`);
+  next();
+});
+
+// Route to handle requests for postcode data
 app.get('/api/postcode', async (req, res) => {
   try {
-    const postcode = req.query.postcode;
+    const postcode = req.query.postcode; // Extract postcode from query parameters
+    // Make a request to the postcode API to retrieve data
     const response = await axios.get(`http://api.postcodes.io/postcodes/${postcode}`);
-    res.json(response.data);
+    console.log(response.data); // Log the data received from the postcode API
+    res.json(response.data); // Send the data back to the client as JSON
   } catch (error) {
+    // Log any errors that occur during the request to the postcode API
     console.error('Error fetching postcode data', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send('Internal Server Error'); // Send a 500 error response
   }
 });
 
+// Route to handle requests for crime data based on location and date
 app.get('/api/crime', async (req, res) => {
-    try {
-      const { date, lat, lng } = req.query;
-      const response = await axios.get(`https://data.police.uk/api/crimes-at-location?date=${date}&lat=${lat}&lng=${lng}`);
-      res.json(response.data);
-    } catch (error) {
-      console.error('Error fetching crime data', error);
-      res.status(500).send('Internal Server Error');
+  try {
+    const date = req.query.date; // Extract date from query parameters
+    const lat = req.query.lat; // Extract latitude from query parameters
+    const lng = req.query.lng; // Extract longitude from query parameters
+
+    // Build the URL for the crime data API request
+    const url = `https://data.police.uk/api/crimes-at-location?date=${date}&lat=${lat}&lng=${lng}`;
+    console.log(`Requesting crime data from URL: ${url}`);
+    // Make a request to the crime data API to retrieve data
+    const response = await axios.get(url);
+    console.log('Crime data response:', response.data); // Log the data received from the crime data API
+
+    res.json(response.data); // Send the crime data back to the client as JSON
+  } catch (error) {
+    // Log any errors that occur during the request to the crime data API
+    console.error('Error fetching crime data', error.message);
+    if (error.response) {
+      // Log the error response from the API if available
+      console.error('Error response:', error.response.data);
     }
-  });
+    res.status(500).send('Internal Server Error'); // Send a 500 error response
+  }
+});
 
-const PORT = process.env.PORT || 3000;
+// Set the port for the server to listen on
+const PORT = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
@@ -44,35 +86,3 @@ app.listen(PORT, () => {
 
 
 
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const axios = require('axios');
-// const app = express();
-
-// app.use(express.static('public'));
-
-// app.get('/postcode', async (req, res) => {
-//     try {
-//         const postcode = req.query.postcode;
-//         if (!postcode) {
-//           return res.status(400).send('Postcode is required');
-//     }
-
-//     const response = await axios.get(`http://api.postcodes.io/postcodes/${postcode}`);
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
